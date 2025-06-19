3d303aeda6e619f368c9956ff83c21fea38d5652
CollinSteeg <167075925+CollinSteeg@users.noreply.github.com>
2025-06-19 13:03:25 +0200

    Added legacy website

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/legacy', function () {
+    return view('legacy.index.html');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
