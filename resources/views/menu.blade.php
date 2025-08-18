ea7666681eeba18e8df12a1ba5460026a091abeb
CollinSteeg <167075925+CollinSteeg@users.noreply.github.com>
2025-08-18 13:22:39 +0200

    Menu for customers

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -1,6 +1,34 @@
 <x-app-layout>
     <x-navbar-with-border>
-        <img class="w-full mb-4" src="menucards/restaurant-menukaart-1-2.jpg" alt="Menukaart 1-2">
-        <img class="w-full" src="menucards/restaurant-menukaart-1.jpg" alt="Menukaart 1">
+        <div class="p-4 bg-white rounded shadow text-sm font-serif max-w-6xl mx-auto">
+            @foreach($menuItems as $category => $items)
+                <div class="mb-8 break-inside-avoid-column">
+                    <h2 class="text-xl font-bold border-b border-gray-300 mb-2">{{ strtoupper($category) }}</h2>
+                    <ul>
+                        @foreach($items as $item)
+                            <li class="flex justify-between py-1 border-b border-dotted border-gray-300">
+                                <span>
+                                    @if($item->menu_number)
+                                        {{ $item->menu_number }}
+                                        @if($item->menu_suffix)
+                                            {{ $item->menu_suffix }}
+                                        @endif.
+                                    @else
+                                        @if($item->menu_suffix)
+                                            {{ $item->menu_suffix }} .
+                                        @endif
+                                    @endif
+                                    {{ $item->name }}
+                                    @if($item->description)
+                                        <span class="text-gray-500 italic">({{ $item->description }})</span>
+                                    @endif
+                                </span>
+                                <span>€ {{ number_format($item->price, 2, ',', '.') }}</span>
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endforeach
+        </div>
     </x-navbar-with-border>
 </x-app-layout>
