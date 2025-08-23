49aac02d00663bac40cf472b6727d82686b0877d
Niels Bouwman <nsw.bouwman@student.avans.nl>
2025-08-23 02:26:32 +0200

    More hidden title Translation

--- resources/lang/en/menu.php
+++ resources/lang/en/menu.php
@@ -4,4 +4,6 @@ return [
     'standard-sort' => 'Default Order',
     'favorites-first-number' => 'Favorites First (Number)',
     'favorites-first-alphabetical' => 'Favorites First (Alphabetical)',
+    'remove-from-favorites' => 'Remove from favorites',
+    'add-to-favorites' => 'Add to favorites'
 ];
